7d2585672f5546eb921ec04e73a8438574d9215b
yeucodonvn <yeucodonvn@gmail.com>
2023-12-03 01:04:32 +0700

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,8 +1,8 @@
-version=0.8.13
-#version=0.9.4
+#version=0.8.13
+version=0.9.4
 apt-get update && apt-get -y upgrade
 apt-get install gcc make git -y
-wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/raw/master/archive/${version}.tar.gz
+wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
 make -f Makefile.Linux
