6b522faaddd3efc5c7870bc2b5a55b734073188b
yeucodonvn <yeucodonvn@gmail.com>
2023-12-03 00:35:22 +0700

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,7 +1,8 @@
-version=0.8.13
+#version=0.8.13
+version=0.9.4
 apt-get update && apt-get -y upgrade
 apt-get install gcc make git -y
-wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
+wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/raw/master/archive/${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
 make -f Makefile.Linux
@@ -9,12 +10,12 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/yeucodonvn/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 mkdir /var/log/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://github.com/yeucodonvn/3proxy/raw/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
+wget --no-check-certificate  https://raw.github.com/yeucodonvn/3proxy/master/3proxy
 chmod  +x /etc/init.d/3proxy
 update-rc.d 3proxy defaults
